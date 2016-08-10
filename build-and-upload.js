22217fe36ef21dd00c0e5ae3fc50ff37d46adcee
igorklopov <igor@klopov.com>
2016-08-10 16:14:21 +0300

    build fn is localPlace-agnostic and receives copyDest option

--- build-and-upload.js
+++ build-and-upload.js
@@ -1,12 +1,23 @@
+import { localPlace, remotePlace } from './lib/places.js';
 import build from './lib/build.js';
 import chalk from 'chalk';
 import patchesJson from './patches/patches.json';
 import { targets } from './lib/system.js';
+import upload from './lib/upload.js';
+import { version } from './package.json';
+
+const platform = process.platform;
 
 async function main () {
   for (const nodeVersion in patchesJson) {
     for (const target of targets) {
-      await build(nodeVersion, target);
+      const local = localPlace({
+        arch: target, nodeVersion, platform, version });
+      await build({
+        copyDest: local, nodeVersion, target });
+      const remote = remotePlace({
+        arch: target, nodeVersion, platform, version });
+      await upload(local, remote);
     }
   }
 }
