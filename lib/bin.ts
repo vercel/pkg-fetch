cc06aa6639fb8f287fb6ba73e4b3c5056ed9ac1c
Jesse Chan <jc@linux.com>
2021-03-31 22:17:58 +0800

    Switch from minimist to yargs
    Much better with "--help" and default values. It also allows us to set CLI
    options with PKG_FETCH_OPTION_. For instance:
    
    PKG_FETCH_OPTION_n=node14
    PKG_FETCH_OPTION_b=true
    
    builds Node 14 for the host platform and arch. might be useful for Dockerfile-scripts.
    
    We can add descriptions in a later change to make this even more user-friendly.
    
     $ pkg-fetch -h
    Options:
      -n, --node-range                                  [string] [default: "latest"]
      -p, --platform                                     [string] [default: "linux"]
      -a, --arch                                           [string] [default: "x64"]
      -f, --force-fetch                                                    [boolean]
      -b, --force-build                                                    [boolean]
      -v, --version      Show version number                               [boolean]
      -h, --help         Show help                                         [boolean]

--- lib/bin.ts
+++ lib/bin.ts
@@ -1,19 +1,40 @@
 #!/usr/bin/env node
 
-import minimist from 'minimist';
+import yargs from 'yargs';
+
+import { hostPlatform, hostArch } from './system';
 import { log } from './log';
 import { need } from './index';
+import { version } from '../package.json';
 
 async function main() {
-  const argv = minimist(process.argv.slice(2), {
-    boolean: ['f', 'b'],
-    string: ['n', 'p', 'a'],
-  });
-  const nodeRange = argv.n || argv._.shift();
-  const platform = argv.p || argv._.shift();
-  const arch = argv.a || argv._.shift();
-  const forceFetch = argv.f;
-  const forceBuild = argv.b;
+  const { argv } = yargs
+    .env('PKG_FETCH_OPTION_')
+    .option('node-range', { alias: 'n', default: 'latest', type: 'string' })
+    .option('platform', { alias: 'p', default: hostPlatform, type: 'string' })
+    .option('arch', { alias: 'a', default: hostArch, type: 'string' })
+    .option('force-fetch', {
+      alias: 'f',
+      type: 'boolean',
+    })
+    .option('force-build', {
+      alias: 'b',
+      type: 'boolean',
+    })
+    .conflicts('force-fetch', 'force-build')
+    .version(version)
+    .alias('v', 'version')
+    .help()
+    .alias('h', 'help');
+
+  const {
+    'node-range': nodeRange,
+    platform,
+    arch,
+    'force-fetch': forceFetch,
+    'force-build': forceBuild,
+  } = argv;
+
   const local = await need({
     nodeRange,
     platform,
@@ -21,6 +42,7 @@ async function main() {
     forceFetch,
     forceBuild,
   });
+
   log.info(local);
 }
 
