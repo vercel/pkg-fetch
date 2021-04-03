6d676ce3d05bd7066bfcf2d16daf053ad9bf810b
Jesse Chan <jc@linux.com>
2021-04-03 19:51:46 +0800

    places: simplify and remove dependency on "expand-template"

--- typings/expand-template.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'expand-template' {
-  function expandTemplate(): (
-    template: string,
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    variables: object
-  ) => string;
-
-  export default expandTemplate;
-}
