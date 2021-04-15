2a68741776e74f626c83254941a639ab7dcf6332
Jesse Chan <jc@linux.com>
2021-04-15 12:27:39 +0800

    Revert "--without-snapshot for alpine builds as a temporary workaround"
    This reverts commit d4b89a9511cfd87eee8dbe2f7505db6e3c19b3ea.
    
    No longer needed.

--- lib/build.ts
+++ lib/build.ts
@@ -32,9 +32,6 @@ function getConfigureArgs(major: number, targetPlatform: string): string[] {
   args.push('--without-inspector');
 
   if (hostPlatform === 'alpine') {
-    // https://github.com/mhart/alpine-node/blob/base-7.4.0/Dockerfile#L33
-    args.push('--without-snapshot');
-
     // Statically Link against libgcc and libstdc++ libraries. See vercel/pkg#555.
     // libgcc and libstdc++ grant GCC Runtime Library Exception of GPL
     args.push('--partly-static');
