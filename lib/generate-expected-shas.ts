8015eb2f02b59b911a22b7cff78aceb8cad46148
Brad Parham <brad.a.parham@intel.com>
2023-03-27 15:47:38 +0200

    update sha sorting algorithm to coerce to semver sorting

--- lib/generate-expected-shas.ts
+++ lib/generate-expected-shas.ts
@@ -3,6 +3,7 @@ import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import { log } from './log';
 import { version } from '../package.json';
+import { nodeBinarySortFunction } from './utils';
 
 async function main() {
   const { argv } = yargs
@@ -15,6 +16,8 @@ async function main() {
   const shaFileContent = readFileSync(argv.input).toString();
   const shaMap: { [id: string]: string } = {};
   for (const line of shaFileContent.split('\n')) {
+    // Expect line to be of the form
+    // <sha> node-v<version>-host-arch
     const lineParts = line.split(/\s+/);
     if (lineParts.length === 2) {
       // eslint-disable-next-line prefer-destructuring
@@ -24,14 +27,12 @@ async function main() {
 
   // Sort map
   const sortedShaMap: { [id: string]: string } = {};
-  for (const nodeVersion of Object.keys(shaMap).sort()) {
+  for (const nodeVersion of Object.keys(shaMap).sort(nodeBinarySortFunction)) {
     sortedShaMap[nodeVersion] = shaMap[nodeVersion];
   }
 
   writeFileSync(join(__dirname, '../lib/expected-shas.json'),
                 `${JSON.stringify(sortedShaMap, null, 2)}\n`);
-  // console.log(JSON.stringify(sortedShaMap, null, 2));
-
 }
 
 main().catch((error) => {
--- lib/print-hashes.ts
+++ lib/print-hashes.ts
@@ -1,10 +1,11 @@
 import { EXPECTED_HASHES } from './expected';
+import { nodeBinarySortFunction } from './utils';
 
 /* eslint-disable no-console */
 
 function main() {
-  for (const nodeVersion of Object.keys(EXPECTED_HASHES).sort()) {
-    console.log(`${nodeVersion} ${EXPECTED_HASHES[nodeVersion]}`);
+  for (const nodeVersion of Object.keys(EXPECTED_HASHES).sort(nodeBinarySortFunction)) {
+    console.log(`${EXPECTED_HASHES[nodeVersion]} ${nodeVersion}`);
   }
 }
 
